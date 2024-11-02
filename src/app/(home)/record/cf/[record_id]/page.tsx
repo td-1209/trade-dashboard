cd1ca5ffa7bba614bdc32d75815eb434d0e11fae
西郷隆盛 <teon22dean@gmail.com>
2024-11-03 05:21:19 +0900

    環境リセット

--- src/app/(home)/record/cf/[record_id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function Home({ params }: { params: Promise<{ record_id: string }> }) {
-  const recordId = (await params).record_id;
-  return (
-    <>
-      cash-flow-record ({recordId})
-    </>
-  );
-}
