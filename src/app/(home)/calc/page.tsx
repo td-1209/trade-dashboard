24185c1e5ec41cdf78ac486fdd68836f4335fb37
西郷隆盛 <teon22dean@gmail.com>
2024-11-16 19:43:22 +0900

    どの通貨でもロット計算が可能に

--- src/app/(home)/calc/page.tsx
+++ src/app/(home)/calc/page.tsx
@@ -1,79 +1,59 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { calculateMaxLotSize, calculateRequiredMargin } from '@/lib/calc';
-
+import { calculateMaxLotSize } from '@/lib/calc';
 
 export default function Home() {
   const [entryPrice, setEntryPrice] = useState<number>(0);
   const [exitPrice, setExitPrice] = useState<number>(0);
+  const [tradingCapital, setTradingCapital] = useState<number>(0);
   const [currencyAmountPerLot, setCurrencyAmountPerLot] = useState<number>(100000);
   const [maintenanceMarginRatio, setMaintenanceMarginRatio] = useState<number>(20);
-  const [lotSize, setLotSize] = useState<number>(0);
-  const [leverage, setLeverage] = useState<number>(1000);
-  const [tradingCapital, setTradingCapital] = useState<number>(0);
+  const [jpyCrossRate, setJpyCrossRate] = useState<number>(1);
   const [maxLotSize, setMaxLotSize] = useState<number | null>(null);
-  const [requiredMargin, setRequiredMargin] = useState<number | null>(null);
   useEffect(() => {
     const maxlotSize =  calculateMaxLotSize({
-      tradingCapital: tradingCapital,
       entryPrice: entryPrice,
       exitPrice: exitPrice,
+      tradingCapital: tradingCapital/jpyCrossRate,
       currencyAmountPerLot: currencyAmountPerLot,
       maintenanceMarginRatio: maintenanceMarginRatio
     });
     setMaxLotSize(maxlotSize);
-    const requiredMargin =  calculateRequiredMargin({
-      entryPrice: entryPrice,
-      currencyAmountPerLot: currencyAmountPerLot,
-      lotSize: lotSize,
-      leverage: leverage
-    });
-    setRequiredMargin(requiredMargin);
   }, [
     entryPrice,
     exitPrice,
+    tradingCapital,
+    jpyCrossRate,
     currencyAmountPerLot,
     maintenanceMarginRatio,
-    lotSize,
-    leverage,
-    tradingCapital
   ]);
   return (
     <>
       <div className='px-5 py-5'>
         <div className='flex space-x-5'>
           <div className='flex-1'>
-            <InputField label={'通貨/ロット'} value={currencyAmountPerLot.toString()} setValue={(value: number) => setCurrencyAmountPerLot(value)} />
+            <InputField label={'通貨数/ロット'} value={currencyAmountPerLot.toString()} setValue={(value: number) => setCurrencyAmountPerLot(value)} />
           </div>
           <div className='flex-1'>
-            <InputField label={'維持率(%)'} value={maintenanceMarginRatio.toString()} setValue={(value: number) => setMaintenanceMarginRatio(value)} />
-          </div>
-          <div className='flex-1'>
-            <InputField label={'レバレッジ'} value={leverage.toString()} setValue={(value: number) => setLeverage(value)} />
+            <InputField label={'証拠金維持率(%)'} value={maintenanceMarginRatio.toString()} setValue={(value: number) => setMaintenanceMarginRatio(value)} />
           </div>
         </div>
         <div className='flex space-x-5'>
           <div className='flex-1'>
-            <InputField label={'注文額'} value={entryPrice.toString()} setValue={(value: number) => setEntryPrice(value)} />
+            <InputField label={'決済通貨/JPY'} value={jpyCrossRate.toString()} setValue={(value: number) => setJpyCrossRate(value)} />
           </div>
           <div className='flex-1'>
-            <InputField label={'決済額'} value={exitPrice.toString()} setValue={(value: number) => setExitPrice(value)} />
-          </div>
-        </div>
-        <div className='flex space-x-5'>
-          <div className='flex-1'>
-            <InputField label={'ロット'} value={lotSize.toString()} setValue={(value: number) => setLotSize(value)} />
+            <InputField label={'注文レート'} value={entryPrice.toString()} setValue={(value: number) => setEntryPrice(value)} />
           </div>
           <div className='flex-1'>
-            <InputField label={'有効証拠金'} value={tradingCapital.toString()} setValue={(value: number) => setTradingCapital(value)} />
+            <InputField label={'損切レート'} value={exitPrice.toString()} setValue={(value: number) => setExitPrice(value)} />
           </div>
         </div>
-        <div className='flex space-x-5 pt-10'>
-          { maxLotSize !== null && (<p className='text-xl'>ロット上限： {maxLotSize}</p>) }
-        </div>
-        <div className='flex space-x-5'>
-          { requiredMargin !== null && (<p className='text-xl'>必要証拠金： {requiredMargin}</p>) }
+        <InputField label={'有効証拠金(JPY)'} value={tradingCapital.toString()} setValue={(value: number) => setTradingCapital(value)} />
+        <p>資金管理に基づく1回分の投入額とすること</p>
+        <div className='pt-10'>
+          { maxLotSize !== null && <InputField label={'ロット上限'} value={maxLotSize.toFixed(3)} setValue={(value: number) => setMaxLotSize(value)} /> }
         </div>
       </div>
     </>
