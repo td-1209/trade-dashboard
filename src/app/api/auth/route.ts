c3b7c6e8a4500b5e4b7091f7f30d4808fd52da74
西郷隆盛 <teon22dean@gmail.com>
2025-08-25 01:45:35 +0900

    不要ファイル削除

--- src/app/api/auth/route.ts
+++
@@ -1,9 +0,0 @@
-export const GET = () => {
-  return new Response('', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate':
-        'Basic realm="Access to the staging site", charset="UTF-8"',
-    },
-  });
-};
