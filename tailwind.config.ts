4623a5a1354fe7baafbd2dc3c2277319c8e5ec05
西郷隆盛 <teon22dean@gmail.com>
2025-08-25 14:14:47 +0900

    src/にコードを集約

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,6 @@ export default {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
